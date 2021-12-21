774f2e7afec8d685f0a390af5b0f6baa75dea45a
MordenWebDev <95534788+MordenWebDev@users.noreply.github.com>
2021-12-21 21:26:13 +0530

    v2

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-var ctabutton=document.querySelector(".btn").addEventListener("click",veryfy);
+var ctabutton=document.querySelector(".cta__button").addEventListener("click",veryfy);
 
 
 function veryfy(){
    
 //    document.querySelector(".error").classList.add("opa");
  
-    let x = document.getElementById("input").value;
+    let x = document.getElementById("cta__email").value;
     console.log(x);
     if (x == "") {
-        document.querySelector(".error").classList.add("opa");
+        document.querySelector(".error").classList.add("error--opacity1");
       return false;
     }else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(x))
     {
-        document.querySelector(".error").classList.remove("opa");
+        document.querySelector(".error").classList.remove("error--opacity1");
       return (true)
       
     }
-    document.querySelector(".error").classList.add("opa");
+    document.querySelector(".error").classList.add("error--opacity1");
       return (false)
   }
   
